57b893e4663989499f3147d2db7586c12f0cb80b
Arthur Skowronek <arthur.skowronek13@gmail.com>
2025-08-18 12:04:27 +0200

    Création d'un executable

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from config import set_up_environment, loadCore, start_new_session_get_statistic
 
 from config import log_error
 
+# Comment pour créer l'executable: pyinstaller --onefile --icon=icon_worm_detection_analysis.ico --name=Worm_detection main.py
+
 def main():
     # Setup environment
     set_up_environment()
